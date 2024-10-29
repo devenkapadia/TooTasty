7f6f98edfa8655dc3e3c11f33411f5e224f4775b
Deven Kapadia <devenkapadia1@gmail.com>
2024-10-29 15:18:44 +0530

    Project Setup

---
+++ src/test/java/com/foodisgood/tootasty/TooTastyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foodisgood.tootasty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TooTastyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
